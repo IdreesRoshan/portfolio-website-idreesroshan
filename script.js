335f0ab24774250611496a20b4269e31573e807d
IdreesRoshan <idrees.roshan@outlook.com>
2025-01-09 20:21:36 +0000

    Experience section format complete

--- script.js
+++ script.js
@@ -23,11 +23,34 @@ const observer = new IntersectionObserver((entries) => {
         console.log(entry)
         if(entry.isIntersecting) {
             entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
         }
     });
 });
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show-timeline-left');
+        }
+    });
+});
+
+const hiddentimelineleft = document.querySelectorAll('.hidden-timeline-left');
+hiddentimelineleft.forEach((el) => observer2.observe(el));
+
+const observer3 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show-timeline-right');
+        }
+    });
+});
+
+const hiddentimelineright = document.querySelectorAll('.hidden-timeline-right');
+hiddentimelineright.forEach((el) => observer3.observe(el));
